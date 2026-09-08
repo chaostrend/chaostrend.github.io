24ec47b2b894e7db848c564c6fc94ca5214e1bf1
chaostrend <orientobchod@gmail.com>
2026-09-08 11:20:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -220,7 +220,7 @@ function displayMainVideo(video) {
 // ============================================================
 
 const chaosTrendJingle =
-    new Audio("znelka.m4a");
+    new Audio("znelka.mp3");
 
 
 chaosTrendJingle.preload =
